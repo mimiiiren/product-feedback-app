c768cefc280403116340390fddb304c923de3b7d
mimiiiren <mimiren015@gmail.com>
2026-04-14 14:28:25 -0400

    delete config file

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,8 @@
 import express from "express";
 import pg from "pg";
-import config from "./config.js";
 
 const db = new pg.Pool({
-  connectionString: config.databaseUrl + "&uselibpqcompat=true",
+  connectionString: process.env.DATABASE_URL,
   ssl: true,
 });
 
