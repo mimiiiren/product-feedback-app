ae5cfcb00d6d0ef1239517e5f570e7b55d52cdfa
mimiiiren <mimiren015@gmail.com>
2026-04-15 12:06:52 -0400

    add render link

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000/",
+        target: "https://product-feedback-app-2thw.onrender.com",
         changeOrigin: true,
         secure: false,
       },
